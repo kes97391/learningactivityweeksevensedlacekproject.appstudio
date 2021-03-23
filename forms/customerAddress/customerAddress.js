012e4e7b1cedd1fe6c7dc8d50e850d2d3137f473
kes97391 <79420804+kes97391@users.noreply.github.com>
2021-03-22 20:29:44 -0500

    More updates

--- forms/customerAddress/customerAddress.js
+++ forms/customerAddress/customerAddress.js
@@ -1,4 +1,4 @@
-customerAdd.onshow=function(){
+customerSelect.onshow=function(){
   query = "SELECT * FROM customer"
   req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=kes97391&pass=" + pw + "&database=kes97391&query=" + query)
 
@@ -6,35 +6,23 @@ customerAdd.onshow=function(){
         results = JSON.parse(req.responseText)
         console.log(`the results are \n ${results}`)
         if (results.length == 0)    
-           lblCustomerAdd.value = "There are no customers in the database."
+           lblResults.value = "There are no customers in the database."
         else {        
            let message = ""
            for (i = 0; i < results.length; i++)
                message = message + results[i][1] + "\n"
-           txtaAddCustomer.value = message
+           txtaDisplayCustomers.value = message
         } // end else
 
     } else   // the transit didn't work - bad wifi? server turned off?
-        lblCustomerAdd.value = "Error code: " + req.status
+        lblResults.value = "Error code: " + req.status
 }
 
-
-btnAddCustomer.onclick=function(){
-  let customerName = ""
-  customerName = inptCustomerName.value
-  let customerStreet = ""
-  customerStreet = inptStreet.value
-  let customerCity = ""
-  customerCity = inptCity.value
-  let customerState = ""
-  customerState = inptState.value
-  let customerZip = ""
-  customerZip = inptZipcode.value
-  query = "INSERT INTO customer(name, street, city, state, zipcode) VALUES ('" + customerName + "','" + customerStreet + "','" + customerCity + "','" + customerState + "','" + customerZip + "')"   
+btnUpdate.onclick=function(){
+  let newName = ""
+  newName = inputNewName.value
+  let oldName = ""
+  oldName = inputOldName.value
+  query = "UPDATE customer SET name = '" + newName + "'WHERE name = '"+ oldName + "'"
   req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
 }
-
-
-
-
-
