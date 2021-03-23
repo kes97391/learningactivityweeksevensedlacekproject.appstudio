2a6526bde63af6c98551e83637462438a782a512
kes97391 <79420804+kes97391@users.noreply.github.com>
2021-03-22 21:11:50 -0500

    Final changes

--- forms/customerUpdate/customerUpdate.js
+++ forms/customerUpdate/customerUpdate.js
@@ -1,4 +1,4 @@
-customerSelect.onshow=function(){
+customerUpdate.onshow=function(){
   query = "SELECT * FROM customer"
   req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=kes97391&pass=" + pw + "&database=kes97391&query=" + query)
 
@@ -6,23 +6,25 @@ customerSelect.onshow=function(){
         results = JSON.parse(req.responseText)
         console.log(`the results are \n ${results}`)
         if (results.length == 0)    
-           lblResults.value = "There are no customers in the database."
+           lblUpdateLabel.value = "There are no customers in the database."
         else {        
            let message = ""
            for (i = 0; i < results.length; i++)
                message = message + results[i][1] + "\n"
-           txtaDisplayCustomers.value = message
+           txtaUpdateCustomer.value = message
         } // end else
 
     } else   // the transit didn't work - bad wifi? server turned off?
-        lblResults.value = "Error code: " + req.status
+        lblUpdateLabel.value = "Error code: " + req.status
 }
 
 btnUpdate.onclick=function(){
   let newName = ""
-  newName = inputNewName.value
+  newName = inptNewName.value
   let oldName = ""
-  oldName = inputOldName.value
+  oldName = inptOldName.value
   query = "UPDATE customer SET name = '" + newName + "'WHERE name = '"+ oldName + "'"
   req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
 }
+
+
